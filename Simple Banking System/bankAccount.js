350bcd9554e7999d57a4247c47112eb6b11ddd8e
enochss <enochsaravanamuttu@gmail.com>
2024-08-29 19:45:42 +0100

    Add files via upload

---
+++ Simple Banking System/bankAccount.js
@@ -0,0 +1,7 @@
+class BankAccount {
+    constructor(accountNumber, accountHolder, balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+}
---
+++ Simple Banking System/index.js
@@ -0,0 +1,5 @@
+import {BankAccount} from "./bankAccount";
+
+const myAccount = new BankAccount(199, "enoch", "$-12")
+const davesAccount = new BankAccount(200, "dave", "$90")
+const anniesAccount = new BankAccount(201, "annie", "$10000")
